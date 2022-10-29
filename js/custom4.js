01251ec422cc8e9957119875fc9ddad9be1d16d4
webozza <webozza@gmail.com>
2022-10-29 20:06:03 +0800

    testing form validation

--- js/custom4.js
+++ js/custom4.js
@@ -38,24 +38,9 @@ setTimeout(() => {
     $(".woocommerce-message a").css("color", "red");
   } else if (currentUrl.indexOf("edit-address/billing") > -1) {
     // Edit Address/Billing
-    $("input#billing_phone").attr("onkeypress", "validate(event)");
-    function validate(evt) {
-      var theEvent = evt || window.event;
-
-      // Handle paste
-      if (theEvent.type === "paste") {
-        key = event.clipboardData.getData("text/plain");
-      } else {
-        // Handle key press
-        var key = theEvent.keyCode || theEvent.which;
-        key = String.fromCharCode(key);
-      }
-      var regex = /[0-9]|\./;
-      if (!regex.test(key)) {
-        theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
-      }
-    }
-    validate(evt);
+    $("input#billing_phone").attr(
+      "oninput",
+      "this.value = this.value.replace(/[^0-9.]/g, '').replace(/(..*?)..*/g, '$1')"
+    );
   }
 }, 200);
