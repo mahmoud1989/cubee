64648fbe2664e759e1841df8e20aa4eda86e0c3e
webozza <webozza@gmail.com>
2022-10-29 19:46:43 +0800

    form validation

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,39 @@ setTimeout(() => {
   if (errorMsg.indexOf("account is already registered") > -1) {
     $("input#reg_email").parent().append(theMsg);
   }
+  if (errorMsg.indexOf("Coupon") > -1) {
+    $("input#coupon_code").after(theMsg);
+  }
 
   // Password field validation
   $(".woocommerce-form-login input#password").attr("maxlength", "8");
   $(".woocommerce-form-login input#password").attr("minlength", "8");
 }, 1000);
+
+// Edit Account Page
+let currentUrl = window.location.href;
+if (currentUrl.indexOf("edit-account") > -1) {
+  $('label[for="password_current"]').text("Current password");
+  $('label[for="password_1"]').text("New password");
+} else if (currentUrl.indexOf("customer-logout") > -1) {
+  $(".woocommerce-message a").css("color", "red");
+} else if (currentUrl.indexOf("edit-address/billing")) {
+  $("input#billing_phone").attr("onkeypress", "validate(event)");
+  function validate(evt) {
+    var theEvent = evt || window.event;
+
+    // Handle paste
+    if (theEvent.type === "paste") {
+      key = event.clipboardData.getData("text/plain");
+    } else {
+      // Handle key press
+      var key = theEvent.keyCode || theEvent.which;
+      key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+      theEvent.returnValue = false;
+      if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
+}
